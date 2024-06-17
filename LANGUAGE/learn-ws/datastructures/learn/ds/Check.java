7005a162734db91cf08b9449ff6a0bd58b262041
kailashs <kailash.patisah@ivycomptech.com>
2024-06-17 14:28:14 +0530

    static final fields

--- LANGUAGE/learn-ws/datastructures/learn/ds/Check.java
+++ LANGUAGE/learn-ws/datastructures/learn/ds/Check.java
@@ -1,14 +1,21 @@
 package learn.ds;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Check {
-    public static void main(String[] args) {
-       
-    }
+	
 
+    
 
+    
   
-
+    
+     
+     public static void main(String[] args) {
+     	
+     	
+        System.out.println("here ended");
+     }
 
 } 
