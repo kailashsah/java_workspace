9003058d4d78ae0b4d94d37b21cc0657fe7c6320
kailash <kailashpati86@gmail.com>
2025-03-11 13:00:48 +0530

    epochseconds to localdatetime

--- learn-ij/src/main/java/org/learnjava/p01_lang/S54_TimeFunctions.java
+++ learn-ij/src/main/java/org/learnjava/p01_lang/S54_TimeFunctions.java
@@ -1,15 +1,11 @@
 package org.learnjava.p01_lang;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-class TimeFunctions
-{
-    public void ZonedTime(){
+class TimeFunctions {
+    public void ZonedTime() {
         Instant now = Instant.now();
 
         // convert Instant to ZonedDateTime
@@ -18,23 +14,53 @@ class TimeFunctions
         System.out.println(dtf.format(zonedDateTime)); //2025/03/09 12:15:49   <-- IST
     }
 
-    public void localTime(){
+    public void localTime() {
         LocalDateTime dateTime = LocalDateTime.now();
         String localTime = dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
         System.out.println(localTime); //09/03/25, 12:18 pm
     }
+
+    public void epochToLocalDateTime() {
+
+        //1. setup start time
+        long epochSecond = Instant.now().getEpochSecond();
+        //LocalDateTime dt = new LocalDateTime(Instant.ofEpochSecond(epochSecond), ZoneId.systemDefault());
+        //ctor is private, so use below
+
+        LocalDateTime dt = Instant.ofEpochSecond(epochSecond).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        String dateTime = dt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        System.out.println(dateTime);
+
+        //2. setup end time
+        long epochSecond2 = Instant.now().getEpochSecond() + 5;
+
+        LocalDateTime dt2 = Instant.ofEpochSecond(epochSecond2).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        dateTime = dt2.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        System.out.println(dateTime);
+
+        /*
+             output -  it has the gap of 5 seconds
+             11-Mar-2025, 12:51:08 pm
+             11-Mar-2025, 12:51:13 pm
+        */
+
+    }
+
     public void basicFunctions() {
         long currentTime = System.currentTimeMillis();
         System.out.println("Current time in milliseconds: " + currentTime);
     }
 }
+
 public class S54_TimeFunctions {
     public static void main(String[] args) {
         System.out.println("main starts");
 
         TimeFunctions tf = new TimeFunctions();
-        tf.ZonedTime();
-        tf.localTime();
+        //tf.ZonedTime();
+        //tf.localTime();
+        tf.epochToLocalDateTime();
+
 
         System.out.println("main ends");
     }
