e73494667c5d79e9a425d8817f0da717b629abc4
kailash <kailashpati86@gmail.com>
2025-03-17 17:35:20 +0530

    override

---
+++ learn-ij/src/main/java/org/learnjava/p03_oops/S22_Override.java
@@ -0,0 +1,35 @@
+package org.learnjava.p03_oops;
+
+
+/*
+    1. upcasting and downcasting
+    2. same method could be defined again in the subclass without @Override annotation
+*/
+
+class A {
+
+    public void methodA() {
+        System.out.println("A");
+    }
+}
+
+class B extends A {
+
+    @Override
+    public void methodA() {
+        System.out.println("B");
+    }
+}
+
+
+public class S22_Override {
+    public static void main(String[] args) {
+        //1.
+        A a = new B();
+        a.methodA();    // B
+
+        //2.  down casting
+        //B b = (B) new A(); // java.lang.ClassCastException --> A cannot be cast to class org.learnjava.p01_lang.B
+        //b.methodB();
+    }
+}
