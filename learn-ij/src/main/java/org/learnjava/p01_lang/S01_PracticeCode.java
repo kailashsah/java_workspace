570608652edb616b5436cd8aa4ef73f419f58a89
kailash <kailashpati86@gmail.com>
2025-04-16 18:41:08 +0530

    practice code template

--- learn-ij/src/main/java/org/learnjava/p01_lang/S01_PracticeCode.java
+++ learn-ij/src/main/java/org/learnjava/p01_lang/S01_PracticeCode.java
@@ -1,15 +1,26 @@
 package org.learnjava.p01_lang;
+//import org.apache.commons.lang3.time.DateFormatUtils;
+
+
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 
 public class S01_PracticeCode {
-    public static void main(String[] args)  {
+
+    void practice() {
+
+    }
+
+    public static void main(String[] args) {
         System.out.println("start of prg");
+
+        new S01_PracticeCode().practice();
+
 
         System.out.println("end of prg");
     }
 
-
 }
-
 
 
 
